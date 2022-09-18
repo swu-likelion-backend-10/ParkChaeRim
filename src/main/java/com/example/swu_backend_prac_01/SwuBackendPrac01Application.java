d06cc8c2bdf8ae6f74b436d51034a81e15988ddf
chaerim <qkrdbsk28@naver.com>
2022-09-18 15:19:13 +0900

    2주차 강의 과제

---
+++ src/main/java/com/example/swu_backend_prac_01/SwuBackendPrac01Application.java
@@ -0,0 +1,13 @@
+package com.example.swu_backend_prac_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwuBackendPrac01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwuBackendPrac01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swu_backend_prac_01/SwuBackendPrac01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swu_backend_prac_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwuBackendPrac01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
